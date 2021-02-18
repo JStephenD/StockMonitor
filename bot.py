d57700493a5f540b9eef08355ad3bc2e39076a27
jstephend <stephenfishlmao@gmail.com>
2021-02-18 16:51:41 +0800

    initial bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord, os, random
+from discord.ext import commands
+
+description = '''A Bot to monitor stocks,
+made for an Upwork job.'''
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+command_prefix = ('??')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+if __name__ == '__main__':
+    bot.run(BOT_TOKEN)
