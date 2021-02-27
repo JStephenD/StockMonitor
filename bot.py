cab4a7067678c054ab5bdeede7f631afd305b5c5
jstephend <stephenfishlmao@gmail.com>
2021-02-27 10:23:24 +0800

    shopee working local

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-import discord, os, random, requests, asyncio
+import discord, os, random, requests, asyncio, io
 from discord.ext import commands
 from bs4 import BeautifulSoup
 from requests_html import AsyncHTMLSession, HTML
+from pyppeteer import launch
 
 description = '''A Bot to monitor stocks,
 made for an Upwork job.'''
@@ -13,19 +14,26 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
-# loop = asyncio.get_event_loop()
-
 #  -----------------------------------------------------------
 async def open_site(ctx, url):
     for _ in range(3):
         try:
-            asession = AsyncHTMLSession()
+            browser = await launch(headless=True, 
+                args=[
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox'
+                ]
+            )
+            pages = await browser.pages()
+            page = pages[0]
             await ctx.send('session start')
-            page = await asession.get(url)
+            await page.goto(url)
             await ctx.send('get')
-            await page.html.arender()
-            await ctx.send('render')
-            return page
+            content = await page.content()
+            await ctx.send('extracting html content')
+            await browser.close()
+            return content
+            
         except Exception:
             pass
     return None
@@ -47,16 +55,33 @@ async def update(ctx, site, url):
     await ctx.send('opening site')
     if page := await open_site(ctx, url):
         await ctx.send('site opened and rendered')
+
+        
+
+        # soup = BeautifulSoup(page, 'html.parser')
+        # s = soup.find('div', class_='_3ZV7fL')
+        # print(s)
     
         if site == 'shopee':
             await ctx.send('shopee detected')
 
-            details = page.html.find('.wPNuIn', first=True)
-            soup = BeautifulSoup(details.html, 'html.parser')
-            title = soup.find('div').find('span').text
-            price = soup.find('div', class_='bBOoii').text
-            price2 = soup.find('div', class_='AJyN7v').text
-            stock = soup.find('div', class_='_2_ItKR').find_all('div')[-1].text
+            # try:
+            #     with io.open('page.html', 'w', encoding="utf-8") as f:
+            #         f.write(page)
+            # except Exception as e:
+            #     print(e)
+
+            soup = BeautifulSoup(page, 'html.parser')
+            details = soup.find('div', class_='_3iw7jx')
+            price, price2 = None, None
+
+
+            title = details.find('div', class_='_3ZV7fL').find('span').text
+            if price_div := details.find('div', class_='bBOoii'):
+                price = price_div.text
+            if price2_div := details.find('div', class_='AJyN7v'):
+                price2 = price2_div.text
+            stock = details.find('div', class_='_2_ItKR').find_all('div')[-1].text
 
             print(f'description: {title}')
             print(f'price: {price}', f'now {price2}' if price2 else '')
@@ -67,7 +92,7 @@ async def update(ctx, site, url):
         if site == 'lazada':
             await ctx.send('lazada detected')
             try:
-                soup = BeautifulSoup(page.content, 'html.parser')
+                soup = BeautifulSoup(page, 'html.parser')
                 title = soup.find('div', id="module_product_title_1").text
                 price = soup.find('span', class_='pdp-price_type_normal').text
                 orig_price = None
@@ -91,4 +116,13 @@ async def ping(ctx):
 def track():
     pass
 
+
 bot.run(BOT_TOKEN)
+
+
+# async def main():
+    # page = await open_site('x', 'https://shopee.ph/Hyundai-Platinum-DM-8000-Professional-Microphone-System-COD-i.39655159.1429657456')
+    # page = await open_site('x', 'https://example.com/')
+    # print(page)
+
+# asyncio.get_event_loop().run_until_complete(main())
